38090c2ab13fc980a6a862b940290c4d930466cd
changizyv <151434499+changizyv@users.noreply.github.com>
2024-03-01 20:26:59 +0330

    Update true mobile format number.py

--- true mobile format number.py
+++ true mobile format number.py
@@ -1,10 +1,10 @@
 import re
 a= input("Please enter your number: ")
-# حذف همه کاراکترهای غیر عددی
+# delete space or string characters
 b= re.sub("^\+98","0",a)
-# تبدیل "+98" در ابتدای رشته به "0"
+# convert "Phone country code" to "0"
 c= re.sub("\D","",b)
-# تبدیل "0098" در ابتدای رشته به "0"
+# convert "0098" to "0"
 d= re.sub("^0098","0",c)
 print ("your number with true format is : " , d)
 
